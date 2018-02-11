8613590a2cde8256a4b9dda16d0ea4c59e581b50
Harmeet Singh(Taara) <harmeetsingh.0013@gmail.com>
2018-02-11 22:24:17 +0530

    Revert "Fix FutOpt examples"

--- src/main/scala/usercases/mtransfomer/Example3.scala
+++ src/main/scala/usercases/mtransfomer/Example3.scala
@@ -1,30 +1,29 @@
 package usercases.mtransfomer
 
-import cats.syntax.applicative._
-import cats.instances.option._
-import cats.instances.future._
-
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Example3 extends App {
 
-  case class FutOpt[A](value: Future[Option[A]])
+  case class FutOpt[A](value: Future[Option[A]]) {
+    def pure[A](a: A): FutOpt[A] = FutOpt(Future.successful(Some(a)))
 
-  object FutOpt {
-    implicit val monadInstance: Monad[FutOpt] = new Monad[FutOpt] {
-      override def pure[A](a: => A): FutOpt[A] =
-        FutOpt(a.pure[Option].pure[Future])
+    def flatMap[A, B](fa: FutOpt[A])(f: A => FutOpt[B]): FutOpt[B] =
 
-      override def map[A, B](f: A => B)(fa: FutOpt[A]): FutOpt[B] =
-        FutOpt(fa.value.map(optA => optA.map(f)))
-
-      override def flatMap[A, B](f: A => FutOpt[B])(fa: FutOpt[A]): FutOpt[B] =
-        FutOpt(fa.value.flatMap(opt => opt match {
-          case Some(a) => f(a).value
-          case None => (None: Option[B]).pure[Future]
-        }))
-    }
   }
 
+  new Monad[FutOpt] {
+
+    def pure[A](a: => A): FutOpt[A] = FutOpt(a.pure[Option].pure[Future])
+
+    def map[A, B](fa: FutOpt[A])(f: A => B): FutOpt[B] =
+      FutOpt(fa.value.map(optA => optA.map(f)))
+
+    def flatMap[A, B](fa: FutOpt[A])(f: A => FutOpt[B]): FutOpt[B] =
+      FutOpt(fa.value.flatMap(opt => opt match {
+        case Some(a) => f(a).value
+        case None => (None: Option[B]).pure[Future]
+      }))
+
+  }
 }
