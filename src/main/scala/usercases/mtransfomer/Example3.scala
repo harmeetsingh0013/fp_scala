2e90427d0e2f8b3c617882b3ca2a8e39940f5444
Gabriele Petronella <gabriele@buildo.io>
2018-02-11 11:03:56 +0100

    Move the monad instance inside the companion object

--- src/main/scala/usercases/mtransfomer/Example3.scala
+++ src/main/scala/usercases/mtransfomer/Example3.scala
@@ -11,19 +11,20 @@ object Example3 extends App {
 
   case class FutOpt[A](value: Future[Option[A]])
 
-  new Monad[FutOpt] {
+  object FutOpt {
+    implicit val monadInstance: Monad[FutOpt] = new Monad[FutOpt] {
+      override def pure[A](a: => A): FutOpt[A] =
+        FutOpt(a.pure[Option].pure[Future])
 
-    override def pure[A](a: => A): FutOpt[A] =
-      FutOpt(a.pure[Option].pure[Future])
+      override def map[A, B](f: A => B)(fa: FutOpt[A]): FutOpt[B] =
+        FutOpt(fa.value.map(optA => optA.map(f)))
 
-    override def map[A, B](f: A => B)(fa: FutOpt[A]): FutOpt[B] =
-      FutOpt(fa.value.map(optA => optA.map(f)))
-
-    override def flatMap[A, B](f: A => FutOpt[B])(fa: FutOpt[A]): FutOpt[B] =
-      FutOpt(fa.value.flatMap(opt => opt match {
-        case Some(a) => f(a).value
-        case None => (None: Option[B]).pure[Future]
-      }))
-
+      override def flatMap[A, B](f: A => FutOpt[B])(fa: FutOpt[A]): FutOpt[B] =
+        FutOpt(fa.value.flatMap(opt => opt match {
+          case Some(a) => f(a).value
+          case None => (None: Option[B]).pure[Future]
+        }))
+    }
   }
+
 }
